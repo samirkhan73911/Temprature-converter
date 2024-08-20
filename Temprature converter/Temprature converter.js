d3e8199ce09ddd143c98fb2dcdbed4cd80b84115
samirkhan73911 <60287860+samirkhan73911@users.noreply.github.com>
2024-08-20 05:39:41 -0700

    Add files via upload

---
+++ Temprature converter/Temprature converter.js
@@ -0,0 +1,53 @@
+
+const subbtn = document.getElementById("subbtn")
+const userinput = document.getElementById("UserinputField")
+const CtoFinputField = document.getElementById("CtoFinputField")
+const FtoCinputField = document.getElementById("FtoCinputField")
+const ReusltField = document.getElementById("Result")
+
+let temp
+
+
+
+subbtn.addEventListener("click", (e) => {
+
+    e.preventDefault()
+    
+    temp = userinput.value
+    temp = Number(temp)
+
+    
+    if (!isNaN(temp)) {
+        tempConvertion(temp)
+         ReusltField.style.display="None"
+       
+    }
+
+    else {
+
+        window.alert("Please Enter a valid temprature")
+        // form.reset();
+
+    }
+
+})
+
+
+function tempConvertion(temp){
+
+
+    if(CtoFinputField.checked){
+        temp=temp*9/5+32
+        ReusltField.textContent=temp.toFixed(1)+"°F"
+        ReusltField.style.display="block"
+        
+    }
+    else if(FtoCinputField.checked){
+        temp=(temp-32)*(5/9)
+         ReusltField.textContent=temp.toFixed(1)+"°C"
+         ReusltField.style.display="block"
+
+
+    }
+
+}
